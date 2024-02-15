fdde80109dded4f8fbdc9f1018c01d6770ad95de
nikhil-shanbhag <nikhilshanbhag35@gmail.com>
2024-02-15 23:29:08 +0530

    Add button

--- formHandling/src/App.jsx
+++ formHandling/src/App.jsx
@@ -1,7 +1,9 @@
+import Form from "./components/Form";
 function App() {
   return (
     <>
       <h1 className="bg-orange-500">Hello</h1>
+      <Form />
     </>
   );
 }
--- formHandling/src/components/Form.jsx
+++ formHandling/src/components/Form.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Form() {
   return (
-    <div>Form</div>
+    <div>
+      <form className='flex flex-col items-center justify-center'>
+        <input type="text" placeholder='Username' />
+        <input type="text" placeholder='Email' />
+        <input type="text" placeholder='Mobile Number' />
+        <input type="text" placeholder='Password' />
+        <input type="text" placeholder='Confirm Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+    </div>
   )
 }
 
